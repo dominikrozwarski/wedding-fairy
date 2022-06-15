9429141a7dbb29ada4fc0254ce67e0bdb01a4b33
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-06-15 13:46:53 +0200

    first commit3

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const show = document.querySelector('.mail');
 const form = document.querySelector('.contact-form');
-const shadow = document.querySelector('.shadow')
-const closeBtn = document.querySelector('.close')
+const shadow = document.querySelector('.shadow');
+const closeBtn = document.querySelector('.close');
+const name = document.querySelector('#fname');
+const phone = document.querySelector('#phone');
+const subject = document.querySelector('#subject');
+const send = document.querySelector('.send');
 
 const showMsg = () => {
-	form.style.zIndex = "100";
-    shadow.style.zIndex="10";
+	form.style.zIndex = '100';
+	shadow.style.zIndex = '10';
 };
 
+const closeMsg = () => {
+	form.style.zIndex = '-100';
+	shadow.style.zIndex = '-100';
+};
 
-const closeMsg = () =>{
-    form.style.zIndex = "-100";
-    shadow.style.zIndex="-100";
-}
+const sendBtn = () => {
+	if (name.value == '' || phone.value == '' || subject || value == '') {
+		console.log('no');
+	}
+};
 
-
+send.addEventListener('click', sendBtn);
 show.addEventListener('click', showMsg);
-closeBtn.addEventListener('click', closeMsg)
+closeBtn.addEventListener('click', closeMsg);
